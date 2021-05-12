65d00de763db065058c34f0e33bdfcf560937acb
tocamgar <tocamgar@gmail.com>
2021-05-12 12:05:09 +0200

    gitignore vdrawtext ffpy.py

---
+++ ffpy.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import subprocess
+ventana = tk.Tk()
+
+canvas1 = tk.Canvas(ventana, width = 300, height = 300, bg = 'gray90', relief = 'raised')
+canvas1.pack()
+
+def ff ():
+  subprocess.call("ffprobe -i /mnt/d/Vídeos/TEST.gif" ,shell=True )
+
+boton01 = tk.Button(text='     ffprobe     ', command = ff, bg = 'red', fg = 'white', font = ('helvetica', 12, 'bold'))
+canvas1.create_window(150, 150, window=boton01)
+
+ventana.mainloop()
